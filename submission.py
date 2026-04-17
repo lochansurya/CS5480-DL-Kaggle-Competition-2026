0d16372f11f2e49278dc0db2e32273c13726a4f3
lochansurya <lochanneeli@gmail.com>
2026-04-17 21:21:42 +0530

    change epochs=50, and batch_size=256

--- submission.py
+++ submission.py
@@ -20,10 +20,10 @@ class CFG:
     seed: int = 42
 
     img_size: int = 224
-    batch_size: int = 128
+    batch_size: int = 256
     num_workers: int = 2
 
-    epochs: int = 30
+    epochs: int = 50
     lr: float = 1e-3
     weight_decay: float = 1e-4
 
